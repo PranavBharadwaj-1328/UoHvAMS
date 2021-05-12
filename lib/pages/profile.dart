fbf6f098778a5709321c87737632cff1e91f5dc3
PranavBharadwaj-1328 <bharadwaj.pranav1328@gmail.com>
2021-05-12 16:51:56 +0530

    Init

---
+++ lib/pages/profile.dart
@@ -0,0 +1,108 @@
+import 'dart:io';
+
+import 'package:face_net_authentication/pages/widgets/app_button.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'home.dart';
+import 'dart:math' as math;
+
+class Profile extends StatelessWidget {
+  const Profile(this.username, {Key key, this.imagePath}) : super(key: key);
+  final String username;
+  final String imagePath;
+
+  final String githubURL =
+      "https://github.com/MCarlomagno/FaceRecognitionAuth/tree/master";
+
+  void _launchURL() async => await canLaunch(githubURL)
+      ? await launch(githubURL)
+      : throw 'Could not launch $githubURL';
+
+  @override
+  Widget build(BuildContext context) {
+    final double mirror = math.pi;
+    return Scaffold(
+      backgroundColor: Color(0XFFFFFFFFF),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: FileImage(File(imagePath)),
+                      ),
+                    ),
+                    margin: EdgeInsets.all(20),
+                    width: 50,
+                    height: 50,
+                    // child: Transform(
+                    //     alignment: Alignment.center,
+                    //     child: FittedBox(
+                    //       fit: BoxFit.cover,
+                    //       child: Image.file(File(imagePath)),
+                    //     ),
+                    //     transform: Matrix4.rotationY(mirror)),
+                  ),
+                  Text(
+                    'Hi ' + username + '!',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFEFFC1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.done,
+                      size: 30,
+                      color: Color(0xFF64DD17),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Your attendance has been registered at the following location: ',
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.left,
+                    )
+                  ],
+                ),
+              ),
+              Spacer(),
+              AppButton(
+                text: "Leave",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()),
+                  );
+                },
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                color: Color(0xFFFF6161),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
