160806feefd90501dfaabeb3be9a9336b88402c3
RohanBera <berarohan88@gmail.com>
2021-05-15 00:34:36 +0530

    Notification service separated

---
+++ lib/services/notification.service.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  NotificationService(this.flutterLocalNotificationsPlugin);
+
+  void notificationInitialize() {
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS =
+        IOSInitializationSettings(onDidReceiveLocalNotification: null);
+    var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: null,
+    );
+  }
+
+  void scheduleNotification(String title, String subtitle) {
+    print("scheduling one with $title and $subtitle");
+    var rng = new Random();
+    Future.delayed(Duration(seconds: 2)).then((result) async {
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'your channel id', 'your channel name', 'your channel description',
+          importance: Importance.high,
+          priority: Priority.high,
+          ticker: 'ticker');
+      var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          android: androidPlatformChannelSpecifics,
+          iOS: iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(
+          rng.nextInt(100000), title, subtitle, platformChannelSpecifics,
+          payload: 'item x');
+    });
+  }
+}
