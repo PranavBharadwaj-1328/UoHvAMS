9237067f37b3fb895d36b560216df55de7959010
RohanBera <berarohan88@gmail.com>
2021-09-01 21:23:21 +0530

    Have to add validation in future. So, changed from 'TextField' to 'TextFormField'

--- lib/pages/widgets/app_text_field.dart
+++ lib/pages/widgets/app_text_field.dart
@@ -16,25 +16,39 @@ class AppTextField extends StatelessWidget {
   final bool autofocus;
   final bool isPassword;
 
+  validate(String text) {
+    if (text.length == 0 || text.isEmpty) {
+      return "Field cannot be empty.";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: this.controller,
       autofocus: this.autofocus,
       cursorColor: Color(0xFF5BC8AA),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Field cannot be empty';
+        }
+        return null;
+      },
       decoration: InputDecoration(
         floatingLabelBehavior: FloatingLabelBehavior.never,
         labelText: labelText,
         border: InputBorder.none,
+        // errorText: validate(this.controller.text),
         filled: true,
         fillColor: Colors.grey[200],
-        enabledBorder: new OutlineInputBorder(
+        enabledBorder: OutlineInputBorder(
           borderSide: BorderSide.none,
           borderRadius: const BorderRadius.all(
             const Radius.circular(10.0),
           ),
         ),
-        focusedBorder: new OutlineInputBorder(
+        focusedBorder: OutlineInputBorder(
           borderSide: BorderSide.none,
           borderRadius: const BorderRadius.all(
             const Radius.circular(10.0),
